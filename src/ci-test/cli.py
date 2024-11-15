a5b2f789e1ad4e9aa83597effb3f3e340f48ad88
Evan Kohilas <ekohilas@users.noreply.github.com>
2024-11-15 21:52:58 +1100

    added files for cli

--- src/ci-test/cli.py
+++ src/ci-test/cli.py
@@ -1,10 +1,15 @@
 import sys
 from main import main
 
-if __name__ == "__main__":
+
+def cli():
     if len(sys.argv) != 2:
         print("Usage: python main.py <path_to_json>")
         sys.exit(1)
 
     json_path = sys.argv[1]
     main(json_path=json_path)
+
+
+if __name__ == "__main__":
+    cli()
