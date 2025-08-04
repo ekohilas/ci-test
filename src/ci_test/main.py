9e16183dd9a062e8e6ed867cd5b47944d96265ba
Evan Kohilas <ekohilas@users.noreply.github.com>
2025-08-04 23:16:54 +1000

    add feature flags (#10)

--- src/ci_test/main.py
+++ src/ci_test/main.py
@@ -3,16 +3,18 @@ import sys
 from ci_test import gitlab_ci_local_parser
 from ci_test import rule_collator
 from ci_test import rule_formatter
+from ci_test import flags
 import json
 
 
-def main(json_path: str):
+def main(json_path: str, option_flags: flags.Flags) -> str:
     jsonParser = gitlab_ci_local_parser.JsonParser(
         json_path=json_path,
     )
     jobs = jsonParser.get_jobs()
     ruleCollator = rule_collator.RuleCollator(
         ci_jobs=jobs,
+        option_flags=option_flags,
     )
     jobs_by_rules = ruleCollator.jobs_by_rules()
     ruleFormatter = rule_formatter.RuleFormatter(
