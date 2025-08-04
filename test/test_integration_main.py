05f41d953cc34d1687ffa0ea2ffba7866c2cc1fe
Evan Kohilas <ekohilas@users.noreply.github.com>
2025-08-04 23:14:22 +1000

    add feature flags

--- test/test_integration_main.py
+++ test/test_integration_main.py
@@ -1,13 +1,14 @@
 import unittest
 
 
+from ci_test import flags
 from ci_test import main
 
 
 class MainIntegrationTest(unittest.TestCase):
     def test_main_returns_expected_output(self):
         input_path = "test/input.json"
-        actual_output = main.main(input_path)
+        actual_output = main.main(input_path, option_flags=flags.Flags.NONE)
 
         expected_output_path = "test/output.json"
 
@@ -16,9 +17,9 @@ class MainIntegrationTest(unittest.TestCase):
 
         self.assertEqual(actual_output, expected_output)
 
-    def test_input_job_with_no_rules_outputs_job(self):
+    def test_input_job_with_no_rules_outputs_job_when_given_flag(self):
         input_path = "test/job_no_rules_input.json"
-        actual_output = main.main(input_path)
+        actual_output = main.main(input_path, option_flags=flags.Flags.OUTPUT_JOBS_WITH_NO_RULES)
 
         expected_output_path = "test/job_no_rules_output.json"
 
@@ -26,3 +27,9 @@ class MainIntegrationTest(unittest.TestCase):
             expected_output = f.read()
 
         self.assertEqual(actual_output, expected_output)
+
+    def test_input_job_with_no_rules_outputs_nothing_when_not_given_flag(self):
+        input_path = "test/job_no_rules_input.json"
+        actual_output = main.main(input_path, option_flags=flags.Flags.NONE)
+
+        self.assertEqual(actual_output, "[]")
