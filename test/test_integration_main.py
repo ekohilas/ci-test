3d52b1bc40e434679986301cde011983da9dc721
Evan Kohilas <ekohilas@users.noreply.github.com>
2025-08-27 15:27:38 +1000

    add invalid test

--- test/test_integration_main.py
+++ test/test_integration_main.py
@@ -33,3 +33,14 @@ class MainIntegrationTest(unittest.TestCase):
         actual_output = main.main(input_path, option_flags=flags.Flags.NONE)
 
         self.assertEqual(actual_output, "[]")
+
+    def test_input_with_multiple_jobs_is_sorted(self):
+        input_path = "test/multiple_rules_sorted_input.json"
+        actual_output = main.main(input_path, option_flags=flags.Flags.NONE)
+
+        expected_output_path = "test/multiple_rules_sorted_output.json"
+
+        with open(expected_output_path) as f:
+            expected_output = f.read()
+
+        self.assertEqual(actual_output, expected_output)
